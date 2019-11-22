c6c742d43b37a7fe033c86e9a209673800fd2aa2
Benny Schneider <benny.schneider@rewe-digital.com>
2019-11-22 13:22:03 +0100

    Add bottom nav behaviour to notifications link

--- common/navigation/src/main/java/com/stupacki/sample/app/navigation/notifications/NotificationsNavigator.kt
+++ common/navigation/src/main/java/com/stupacki/sample/app/navigation/notifications/NotificationsNavigator.kt
@@ -3,10 +3,12 @@ package com.stupacki.sample.app.navigation.notifications
 import androidx.fragment.app.Fragment
 import com.stupacki.sample.app.navigation.R
 import com.stupacki.sample.app.navigation.common.Navigator
+import com.stupacki.sample.app.navigation.main.provider.BottomNavProvider
 
-interface NotificationsNavigator: Navigator {
+interface NotificationsNavigator : Navigator {
 
     fun Fragment.toHomeFeature() {
+        BottomNavProvider.showBottomNav()
         getNavController(this).navigate(R.id.toHomeFeature)
     }
 }
